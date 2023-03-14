9813a16a3950cdba404725bd358e95f073d7eac1
ligangli <804817829@qq.com>
2023-03-14 17:28:56 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ligangli/blog.git master:gh-pages
+# git push -f git@github.com:ligangli/blog.git master:gh-pages
+git subtree push --prefix docs/.vuepress/dist origin gh-pages
 
 
 cd -
